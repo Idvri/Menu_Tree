090efdc4a0a4a6af5cc9a16e6aa704ab1fa44234
Nodar <nodar.panchenko@gmail.com>
2023-12-10 16:25:29 +0400

    Улучшил и закончил механизм отрисовки меню.

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from menu.models import Menu, Item
+
+
 # Register your models here.
+@admin.register(Menu)
+class Menu(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Item)
+class Item(admin.ModelAdmin):
+    list_display = ('name', 'menu', 'item_parent')
--- menu/templatetags/menu_tags.py
+++ menu/templatetags/menu_tags.py
@@ -1,5 +1,4 @@
 from django import template
-from django.core.exceptions import ObjectDoesNotExist
 
 from menu.models import Item
 
@@ -14,20 +13,27 @@ def get_submenu(menu_name, item_name):
 
         if first_menu is None:
             menu = list(items.filter(item_parent__name=None))
+            if second_menu is None:
+                return menu
+            try:
+                menu.insert(menu.index(second_menu[0].item_parent) + 1, second_menu)
+            except ValueError:
+                submenu = get_menu(first_menu=second_menu[0].item_parent, second_menu=second_menu)
+                menu = get_menu(second_menu=submenu)
+            return menu
+
+        elif first_menu and second_menu:
+            first_menu = list(items.filter(name=first_menu))[0]
+            menu = list(items.filter(item_parent__name=first_menu.item_parent))
+            menu.insert(menu.index(second_menu[0].item_parent) + 1, second_menu)
+            return menu
+
         else:
             menu = list(items.filter(item_parent__name=first_menu))
-
-        try:
-            print(menu.index(second_menu[0].item_parent))
-            menu.insert(menu.index(second_menu[0].item_parent) + 1, second_menu)
-        except (IndexError, TypeError):
-            pass
-
-        try:
-            return get_menu(items.get(name=first_menu).item_parent.name, menu)
-        except AttributeError:
-            return get_menu(second_menu=menu)
-        except ObjectDoesNotExist:
+            if not menu:
+                child = list(items.filter(name=first_menu))
+                return get_menu(first_menu=child[0].item_parent)
+            menu = get_menu(second_menu=menu)
             return menu
 
     if menu_name == item_name:
